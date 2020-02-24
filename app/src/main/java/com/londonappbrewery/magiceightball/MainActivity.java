7fc87d033c5653d8aa7d5d96c6782a8c076e4eb4
Mihai Priboiu <priboiumihai@gmail.com>
2020-02-24 20:45:38 +0200

    Inainte de an doilea layout

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,45 @@
 package com.londonappbrewery.magiceightball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private static String TAG = "Ballz";
+    boolean isFirst = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ballButton = findViewById(R.id.btn_ball);
+        final ImageView ballPicture = findViewById(R.id.img_ball);
+        final int[] images = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+        //final boolean isFirst = true;
+
+        ballButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Ze button has been pressed");
+                int number = new Random().nextInt(5);
+                if (isFirst){
+                    while (number == 0){
+                        number = new Random().nextInt(5);
+                    }
+                    isFirst = false;
+                }
+                ballPicture.setImageResource(images[number]);
+            }
+        });
     }
 }
